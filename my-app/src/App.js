aa628ea095eaf09601592aef2fe53e9ae056a2dc
nero010 <assasin.misha.0@mail.rugit config --global core.autocrlf truegit config --global core.safecrlf falsegit config --global user.email assasin.misha.0@mail.ru>
2019-12-24 06:43:08 +0300

    3version

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,25 +18,23 @@ class App extends Component {
       fYear: new Date().getFullYear(),
       Month: new Date().getMonth(),
       fDay: new Date().getDate(),
-      fMonth: 'Декабрь'
+      fMonth: '',
+      monthEdt: []
+
     };
-    
+
+
     this.monthName(new Date().getMonth());
     this.poleLogin = this.state.Login;
     this.polePassword = this.state.Password;
-    this.testLogin = '1';
-    this.testPassword = '1';
+    this.testLogin = 'Admin';
+    this.testPassword = '12345678';
     this.testPole = this.state.test;
 
     this.press = this.press.bind(this);
+
   }
 
-  press() {
-    if (this.poleLogin.value == this.testLogin && this.polePassword.value == this.testPassword)
-      this.setState({ test: this.poleLogin.value });
-    else
-      alert('Неверно введены данные');
-  }
 
   render() {
 
@@ -121,18 +119,82 @@ class App extends Component {
               <div className='CalendarBlock'>
                 <div className='CalendarBlock_head'>
                   <div className='CalendarBlock_head_left'>
-                    <button className='btn01'>Добавить</button>
-                    <button className='btn01'>Обновить</button>
+                    <button className='btn01' >Добавить</button>
+                    <button className='btn01' >Обновить</button>
                   </div>
                   <div className='CalendarBlock_head_ringt'>
+                    <img src='http://www.clker.com/cliparts/r/N/1/v/H/d/magnifying-lens-th.png'></img>
                     <input></input>
                   </div>
                 </div>
-                <div className='CalendarBlock_main'>
+                <div className='CalendarBlock_main_head'>
                   <button onClick={this.dateCreate(-1).bind(this)}>{'<'}</button>
-                  <p>{this.state.fMonth + " " + this.state.fYear}</p>
+                  <div className='CalendarBlock_main_head_text'>
+                    <p>{this.state.fMonth + " " + this.state.fYear}</p>
+                  </div>
                   <button onClick={this.dateCreate(1).bind(this)}>{'>'}</button>
                   <button onClick={this.dateNow().bind(this)}>Сегодня</button>
+                  <div className='CalendarBlock_main'>
+                    <table>
+                      <tr>
+                        <th>Понедельник</th><th>Вторник</th><th>Среда</th><th>Четверг</th><th>Пятница</th><th>Суббота</th><th>Воскресенье</th>
+                      </tr>
+                      <tr>
+                        <td><p>{this.state.monthEdt[1]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[2]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[3]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[4]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[5]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[6]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[7]}</p><textarea rows='7'></textarea></td>
+                      </tr>
+                      <tr>
+                        <td><p>{this.state.monthEdt[1 + 7]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[2 + 7]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[3 + 7]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[4 + 7]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[5 + 7]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[6 + 7]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[7 + 7]}</p><textarea rows='7'></textarea></td>
+                      </tr>
+                      <tr>
+                        <td><p>{this.state.monthEdt[1 + 14]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[2 + 14]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[3 + 14]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[4 + 14]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[5 + 14]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[6 + 14]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[7 + 14]}</p><textarea rows='7'></textarea></td>
+                      </tr>
+                      <tr>
+                        <td><p>{this.state.monthEdt[1 + 21]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[2 + 21]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[3 + 21]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[4 + 21]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[5 + 21]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[6 + 21]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[7 + 21]}</p><textarea rows='7'></textarea></td>
+                      </tr>
+                      <tr>
+                        <td><p>{this.state.monthEdt[1 + 28]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[2 + 28]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[3 + 28]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[4 + 28]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[5 + 28]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[6 + 28]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[7 + 28]}</p><textarea rows='7'></textarea></td>
+                      </tr>
+                      <tr>
+                        <td><p>{this.state.monthEdt[1 + 35]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[2 + 35]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[3 + 35]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[4 + 35]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[5 + 35]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[6 + 35]}</p><textarea rows='7'></textarea></td>
+                        <td><p>{this.state.monthEdt[7 + 35]}</p><textarea rows='7'></textarea></td>
+                      </tr>
+                    </table>
+                  </div>
                 </div>
               </div>
             )
@@ -143,6 +205,57 @@ class App extends Component {
     );
   }
 
+
+
+  getCountToMonth = (date) => {
+    let dates = date;
+    let newDate = new Date(dates.setMonth(dates.getMonth() + 1));
+    newDate.setDate(0);
+    return (newDate.getDate());
+  }
+
+  refreshCalendar = (year1, month1) => {
+
+    let numOfEmptyDays = 0;
+    let mEdt = [];
+    let dates = new Date(year1, month1, 1);
+    switch (dates.getDay()) {
+      case 0: numOfEmptyDays = 6; break;
+      case 1: numOfEmptyDays = 0; break;
+      case 2: numOfEmptyDays = 1; break;
+      case 3: numOfEmptyDays = 2; break;
+      case 4: numOfEmptyDays = 3; break;
+      case 5: numOfEmptyDays = 4; break;
+      case 6: numOfEmptyDays = 5; break;
+    }
+    numOfEmptyDays++;
+    for (let i = 0; i < numOfEmptyDays; i++) {
+      mEdt.push(' ');
+    }
+    let ends = this.getCountToMonth(dates);
+    for (let i = 1; i < ends; i++) {
+      mEdt.push(i);
+    }
+    this.setState({ monthEdt: mEdt });
+    this.setState((state) => {
+
+      return { monthEdt: mEdt }
+    });
+
+  }
+
+  press() {
+    if (this.poleLogin.value == this.testLogin && this.polePassword.value == this.testPassword)
+      this.setState({ test: this.poleLogin.value });
+    else
+      alert('Неверно введены данные');
+    this.monthName(new Date().getMonth());
+
+    this.refreshCalendar(this.state.fYear, this.state.Month);
+
+
+
+  }
 
   monthName = (Month) => {
     switch (Month) {
@@ -165,46 +278,54 @@ class App extends Component {
     var betaMonth = this.state.Month;
     betaMonth += item
     var betaYear = this.state.fYear + item;
+
+
     if (betaMonth > 11) {
-      this.setState({
-        Month: 0,
-        fMonth: "Январь",
-        fYear: betaYear
+      this.setState((state) => {
+        return {
+          Month: 0,
+          fMonth: "Январь",
+          fYear: betaYear
+        }
       });
+      this.refreshCalendar(betaYear, 0);
     } else if (betaMonth < 0) {
-      this.setState({
-        Month: 11,
-        fMonth: "Декабрь",
-        fYear: betaYear
+      this.setState((state) => {
+        return {
+          Month: 11,
+          fMonth: "Декабрь",
+          fYear: betaYear
+        }
       });
-      this.setState({ fYear: betaYear });
+      this.refreshCalendar(betaYear, 11);
     } else {
-      this.setState((State, item) => ({
-        Month: State.Month + item
-      }));
-      this.setState({ Month: betaMonth })
+      this.setState((State) => {
+        return {
+          Month: State.Month + item
+        }
+      });
+      this.setState({ Month: betaMonth });
+      this.refreshCalendar(this.state.fYear, betaMonth);
     }
     this.monthName(betaMonth);
+
   }
-
-
 
   dateNow = (item) => (event) => {
-    this.setState({
-      fYear: new Date().getFullYear(),
-      Month: new Date().getMonth(),
-      fDay: new Date().getDate(),
-      fMonth: "Декабрь"
+    this.setState((state) => {
+      return {
+        fYear: new Date().getFullYear(),
+        Month: new Date().getMonth(),
+        fDay: new Date().getDate(),
+        fMonth: "Декабрь"
+      }
     });
     this.monthName(new Date().getMonth());
+
+    this.refreshCalendar(new Date().getFullYear(), new Date().getMonth());
+
   }
 
-  
-  
-  
-
 }
-
-
 
 export default App;
